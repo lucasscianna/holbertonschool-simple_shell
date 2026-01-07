33b6464153e6fd7da828b61e044485b633e50edb
lucasscianna <sciannalucas0@gmail.com>
2026-01-07 10:15:03 +0000

    ajout des fonctions et fichier path.c pour gere le shell 0.3

--- shell.h
+++ shell.h
@@ -30,4 +30,9 @@ int _strcmp(char *s1, char *s2);
 int is_exit(char *s);
 int _atoi(char *s);
 
+char *get_env(char *name);
+char *find_in_path(char *cmd);
+int _strncmp(char *s1, char *s2, int n);
+int contains_slash(char *s);
+
 #endif
